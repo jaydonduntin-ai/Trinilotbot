b02be6353c6abc012a34e23c682db66626e92151
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-20 09:28:18 -0400

    Register rbx2mm2all command

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,6 +4,7 @@ import { pingCommand } from "./ping.js";
 import { limitedOwnersCommand } from "./limitedowners.js";
 import { rbx2dcCommand } from "./rbx2dc.js";
 import { rbx2mm2Command } from "./rbx2mm2.js";
+import { rbx2mm2AllCommand } from "./rbx2mm2all.js";
 import { robloxCommand } from "./roblox.js";
 import { targetsCommand } from "./targets.js";
 import { scanCommand } from "./scan.js";
@@ -14,6 +15,7 @@ export const commandModules = [
   limitedOwnersCommand,
   rbx2dcCommand,
   rbx2mm2Command,
+  rbx2mm2AllCommand,
   dc2rbCommand,
   alertsCommand,
   targetsCommand,
