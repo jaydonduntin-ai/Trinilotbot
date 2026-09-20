12667c8d5eca007e6810fd1973045caa3eeeff46
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-20 10:02:39 -0400

    Lower MM2 value watch floor to 50k

--- src/monitoring/target-scanner.js
+++ src/monitoring/target-scanner.js
@@ -46,7 +46,7 @@ import {
 
 export const DEFAULT_TARGET_RAP = 450_000;
 export const DEFAULT_TARGET_VALUE = 150_000;
-export const DEFAULT_MM2_VALUE = 100_000;
+export const DEFAULT_MM2_VALUE = 50_000;
 export const DEFAULT_MM2_RAP = 150_000;
 export const DEFAULT_TARGET_COUNT = 10;
 export const MAX_TARGETS = 10;
