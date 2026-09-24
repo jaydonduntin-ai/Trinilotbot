4ee6312a98804a11234afcc14e3743a300227793
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-24 18:32:11 -0400

    Focus bot commands on rbx2dc target scan and limited lookup

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,25 +1,11 @@
-import { alertsCommand } from "./alerts.js";
-import { dc2rbCommand } from "./dc2rb.js";
-import { pingCommand } from "./ping.js";
 import { limitedOwnersCommand } from "./limitedowners.js";
 import { rbx2dcCommand } from "./rbx2dc.js";
-import { rbx2mm2Command } from "./rbx2mm2.js";
-import { rbx2admCommand } from "./game-targets.js";
-import { robloxCommand } from "./roblox.js";
-import { devCommand } from "./dev.js";
+import { scanCommand } from "./scan.js";
 import { targetsCommand } from "./targets.js";
-import { ingameCommand } from "./ingame.js";
 
 export const commandModules = [
-  pingCommand,
-  robloxCommand,
-  limitedOwnersCommand,
   rbx2dcCommand,
-  rbx2mm2Command,
-  rbx2admCommand,
-  dc2rbCommand,
-  alertsCommand,
-  devCommand,
   targetsCommand,
-  ingameCommand,
+  scanCommand,
+  limitedOwnersCommand,
 ];
