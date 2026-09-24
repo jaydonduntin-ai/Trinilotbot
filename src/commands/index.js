b0f0a8114841debeecc3f25078b004f653d6754d
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-24 18:32:34 -0400

    Keep roblox command in focused bot set

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,10 +1,12 @@
 import { limitedOwnersCommand } from "./limitedowners.js";
 import { rbx2dcCommand } from "./rbx2dc.js";
+import { robloxCommand } from "./roblox.js";
 import { scanCommand } from "./scan.js";
 import { targetsCommand } from "./targets.js";
 
 export const commandModules = [
   rbx2dcCommand,
+  robloxCommand,
   targetsCommand,
   scanCommand,
   limitedOwnersCommand,
