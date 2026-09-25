45ea2a6408e346f0a38f7d002a916a36e2f62659
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-25 18:43:06 -0400

    Register dev and verified dc2roblox commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,7 +1,11 @@
+import { dc2robloxCommand } from "./dc2roblox.js";
+import { devCommand } from "./dev.js";
 import { rbx2mm2Command } from "./rbx2mm2.js";
 import { targetsCommand } from "./targets.js";
 
 export const commandModules = [
   targetsCommand,
   rbx2mm2Command,
+  devCommand,
+  dc2robloxCommand,
 ];
