860d8abeb9e28fbecb24612da2f55bb8bfac86ad
jaydonduntin-ai <jaydonduntin@icloud.com>
2026-09-25 21:13:50 -0400

    Register Roblox-to-Discord and Adopt Me commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,15 @@
 import { dc2robloxCommand } from "./dc2roblox.js";
 import { devCommand } from "./dev.js";
+import { rbx2dcCommand } from "./rbx2dc.js";
+import { rbx2admCommand } from "./game-targets.js";
 import { rbx2mm2Command } from "./rbx2mm2.js";
 import { targetsCommand } from "./targets.js";
 
 export const commandModules = [
   targetsCommand,
   rbx2mm2Command,
+  rbx2admCommand,
   devCommand,
   dc2robloxCommand,
+  rbx2dcCommand,
 ];
